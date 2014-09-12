f9a65639dccc3eb74f75a7addc468eda25b6869a
Daniel Rhees <drhees@gmail.com>
2014-09-12 07:29:10 +0000

    complete 02_GroceryList

--- 08-arrays/02_Grocery_List/challenge.js
+++ 08-arrays/02_Grocery_List/challenge.js
@@ -1,3 +1,12 @@
-module.exports.addItem = undefined;
+module.exports.addItem = function(item, itemList){
+				if(itemList.indexOf(item) < 0){
+					itemList.push(item)
+				}
+				return itemList
+			};
 
-module.exports.reverseSortedList = undefined;
+module.exports.reverseSortedList = function(itemList){
+					itemList.sort();
+					itemList.reverse();
+					return itemList
+					};
