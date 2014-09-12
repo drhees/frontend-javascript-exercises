cef277382c5e946a63b20459ec9eee7ae7ed6815
Daniel Rhees <drhees@gmail.com>
2014-09-12 00:06:47 +0000

    complete 08-arrays/00 Array Definitions

--- 08-arrays/00_Array_Definition/challenge.js
+++ 08-arrays/00_Array_Definition/challenge.js
@@ -1,3 +1,8 @@
-module.exports.newArray = undefined;
+module.exports.newArray = function(a1, a2, a3, a4){
+		return [a1, a2, a3, a4];
+		}
 
-module.exports.firstAndLast = undefined;
+module.exports.firstAndLast = function(arr){
+				//return [arr.shift(), arr.pop()];
+				return [arr[0], arr[arr.length - 1]];
+				};
