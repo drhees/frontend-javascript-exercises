bf211b256defd00652651037c21b22b833895c73
Daniel Rhees <drhees@gmail.com>
2014-09-15 16:08:37 +0000

    complete challenge 09-obejects 00-basics

--- 09-objects/00-basics/challenge.js
+++ 09-objects/00-basics/challenge.js
@@ -1,7 +1,42 @@
-module.exports.createCourse = undefined;
+module.exports.createCourse = function(title, duration, students){
+                                    return {
+                                                'title': title,
+                                                'duration': duration,
+                                                'students': students
+                                            }
+                                };
 
-module.exports.addProperty = undefined;
+module.exports.addProperty = function(obj, newProp, newVal) {
+                                obj[newProp] || function(){ obj[newProp] = newVal }();
+                                return obj
+                            }
 
-module.exports.formLetter = undefined;
+module.exports.formLetter = function(msgObj){
+                                
+                                var lineEnding = "\n\n"
+                                
+                                var suffix = function(tokenStr, seperator){
+                                    return tokenStr + seperator
+                                }
+                                var prefix = function(prefixStr, tokenStr){
+                                    return prefixStr + tokenStr
+                                }
 
-module.exports.canIGet = undefined;
+                            return  suffix(prefix("Hello ", msgObj.recipient), "," + lineEnding) +
+                                    suffix(msgObj.msg, lineEnding) +
+                                    prefix("Sincerely,\n", msgObj.sender)
+                                }
+
+
+
+
+module.exports.canIGet = function(item, money){
+                                var items = {
+                                        'MacBook Air' : 999,
+                                        'MacBook Pro' : 1299,
+                                        'Mac Pro' : 2499,
+                                        'Apple Sticker' : 1
+                                        }
+
+                                    return (items[item] && items[item] <= money) || false
+                            };
